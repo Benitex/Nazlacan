43d8ba1c46266f6980862fc0fcc68dfa0adc3283
Benitex <benitoapepe@yahoo.com.br>
2026-01-11 20:11:29 -0300

    Add empty project template

---
+++ Source/Nazlacan/Nazlacan.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Nazlacan.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Nazlacan, "Nazlacan" );
